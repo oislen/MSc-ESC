2a98edc97c959de09122a30f31f62dd8d6d0e988
Oisin <oisin.leonard@gmail.com>
2021-06-06 16:52:37 +0100

    Aded col_names parameter and removed automated code for extracting numeric variables. Stats now generated using the specified input column anmes

--- MSc_2018/Scripts/utilities/numeric_descriptive_statistics.R
+++ MSc_2018/Scripts/utilities/numeric_descriptive_statistics.R
@@ -2,18 +2,16 @@
 library(moments)
 
 # define a function to calculate descriptive statistics for continuous variables
-numeric_descriptive_statistics <- function(dataset, digits = 3, na.rm = TRUE) {
+numeric_descriptive_statistics <- function(dataset, col_names, digits = 3, na.rm = TRUE) {
   # IMPORTANT NOTE: all the descriptive statistics are by default calculated with the NA values removed
   # function that automatically prints relevant descriptive statistics for attributes in a given data set
   # create the data frame to hold the numeric descriptive statistics
   # set the name of the statistics being generated
   stats_names <- c("mean", "variance", "min", "max", "range", "NA %")
-  # extract out the character columns
-  numeric_cols <- names(which(sapply(X = dataset, FUN = function(x) is.numeric(x))))
   # create a data frame of empty
-  numeric_desc_stats <- as.data.frame(matrix(nrow = length(numeric_cols), ncol = length(stats_names), dimnames = list(numeric_cols, stats_names)))
+  numeric_desc_stats <- as.data.frame(matrix(nrow = length(col_names), ncol = length(stats_names), dimnames = list(col_names, stats_names)))
   # iterate through each numeric column
-  for (col in numeric_cols) {
+  for (col in col_names) {
     # next compute the descriptive statistics for the numeric column
     numeric_desc_stats[col, "mean"] <- round(mean(dataset[, col], na.rm = na.rm), digits = digits)
     numeric_desc_stats[col, "variance"] <- round(var(dataset[, col], na.rm = na.rm), digits = digits)
