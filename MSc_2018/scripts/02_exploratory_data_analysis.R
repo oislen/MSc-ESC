8c07398280a04c323b697c52280705ad73cc5851
oislen <oisin.leonard@gmail.com>
2021-06-09 15:43:19 +0100

    Reviewed explanatory comments and restructed code accordingly

--- MSc_2018/scripts/02_exploratory_data_analysis.R
+++ MSc_2018/scripts/02_exploratory_data_analysis.R
@@ -2,7 +2,8 @@
 ## SECTION 2 - EXPLORATORY ANALYSIS ####################################################################################
 ########################################################################################################################
 
-# This section is specifically for visualizing and deriving descriptive statistics on the data
+# This section conducts an exploratory analysis of the raw data.
+# Specifically for visualizing and deriving descriptive statistics on the data
 # In order to understand the underlying the structures and patterns in the data
 
 # A variety of descriptive statistics will be generated for both the numeric data and the categorical data
@@ -16,29 +17,26 @@
 #-- Libraries --#
 
 # Load in relevant libraries
-# igraph will be used for SNA
 library(igraph)
-# ggplot2 will be used for data visualization
 library(ggplot2)
 library(dplyr)
-
-#-- Data --#
-
 # set the working directory
 setwd(file.path(getwd(), 'GitHub/MSc-ESC/MSc_2018'))
-# load in the raw ESC 2016 data for the analysis
-ESCdata <- read.csv(file = "Data/ESC_2016_voting_data.csv", header = T)
 # load in custom utility functions
-source("Scripts/utilities/column_to_factor.R")
 source("Scripts/utilities/factor_descriptive_statistics.R")
 source("Scripts/utilities/numeric_descriptive_statistics.R")
 source("Scripts/utilities/plot_bar_chart.R")
 source("Scripts/utilities/plot_histogram.R")
-source("Scripts/utilities/chisq_assoc_test.R")
 source("Scripts/utilities/plot_scatter.R")
+source("Scripts/utilities/chisq_assoc_test.R")
 source("Scripts/utilities/corr_tests.R")
 source("Scripts/utilities/pred_corr_tests.R")
 source("Scripts/utilities/graph_network.R")
+
+#-- Data --#
+
+# load in the raw ESC 2016 data for the analysis
+ESCdata <- read.csv(file = "data/ESC_2016_voting_data.csv", header = T)
 
 #-- Some Initial Data Processing --#
 
@@ -63,104 +61,87 @@ all_factors <- c(voting_factors, comp_factors, ext_factors, perf_factors)
 #-- Descriptive Statistics --#
 ##############################
 
-# Here I shall derive relevant descriptive statistics for the exploratory analysis
-# To do this I shall define two function which save the descriptive statistics in twp separate data frames
+# Here relevant descriptive statistics are derived for the exploratory analysis
+# Two custom function are used to generate the descriptive statistics in twp separate data frames
 # specifically for categorical data and numeric data
 
-#-- CATEGORICAL VARIABLES --#
-
-# NOTE: could code up a table of descriptive statistics for continuous and discrete variables, metadata
-# factor_descriptive_statistics(dataset = reduceddata)
-full_data_factor_descriptive_statistics_df <- factor_descriptive_statistics(dataset = ESCdata, col_names = all_factors)
-# View(full_data_factor_descriptive_statistics_df)
-write.csv(x = full_data_factor_descriptive_statistics_df, file = "Report/Stats/categorical_descriptive_statistics.csv")
-
-#-- CONTINUOUS FEATURES --#
+# run the categorical descriptive statistics function
+raw_data_cat_desc_stats <- factor_descriptive_statistics(dataset = ESCdata, col_names = all_factors)
+# write the categorical descriptive statistics to disk
+write.csv(x = raw_data_cat_desc_stats, file = "report/stats/categorical_descriptive_statistics.csv")
 
 # generate numeric descriptive statistics
-full_data_numeric_descriptive_statistics_df <- numeric_descriptive_statistics(dataset = ESCdata, col_names = all_num)
-# View(full_data_numeric_descriptive_statistics_df)
-write.csv(x = full_data_numeric_descriptive_statistics_df, file = "Report/Stats/numeric_descriptive_statistics.csv")
+raw_data_num_desc_stats <- numeric_descriptive_statistics(dataset = ESCdata, col_names = all_num)
+# write the numeric descriptive statistics to disk
+write.csv(x = raw_data_num_desc_stats, file = "report/stats/numeric_descriptive_statistics.csv")
 
-##################################################
-#-- Individual Categorical Variable Bar Charts --#
-##################################################
+#################################
+#-- Individual Variable Plots --#
+#################################
 
-# In this section I shall generate a variety of data visualizations
-# This allows us to see the underlying structures within each variable
-# I shall generate bar charts for categorical variables
+# This section generates a variety of data visualizations
+# To display the underlying structures within each variable
+# Bar charts are generated for each categorical variable
+# histograms are generated for each numeric variable
+
 # call bar chart plotting function
-plot_bar_chart(dataset = ESCdata, col_names = all_factors)
-
-#########################################
-#-- Individual Numeric Variable Plots --#
-#########################################
-
-# In this section I shall generate a variety of data visualizations
-# This allows us to see the underlying structures within each variable
-# I shall generate histograms for the numeric variables
+plot_bar_chart(dataset = ESCdata, col_names = all_factors, output_dir = 'report/plots/bar_charts')
 # call histogram plotting function
-plot_histogram(dataset = ESCdata, col_names = all_num)
+plot_histogram(dataset = ESCdata, col_names = all_num, output_dir = 'report/plots/histograms')
 
 ########################################
 #-- CHI-SQUARED TESTS OF ASSOCIATION --#
 ########################################
 
-# In this section I shall conduct chi-squared tests of association
+# This section conducts chi-squared tests of association
 # for each categorical predictor variable and the response variable
 # Hypothesis:
 # Ho: x is independent of y
 # Ha: x is associated with y
 
-# There are two parts to it
-# (1) Create the data frame to store the chi-squared tests
-# (2) Fill the data frame with the relevant information
-
 # perform chi-sq tests of association with Points
 chisqtestdf <- chisq_assoc_test(data = ESCdata, col_names = all_factors)
-
 # write the data frame to a csv file
-write.csv(x = chisqtestdf, file = "Report/Stats/chi_sq_tests_response.csv", row.names = F)
+write.csv(x = chisqtestdf, file = "report/stats/chi_sq_tests_response.csv", row.names = F)
 
-################################################################
-#-- Correlation Scatter Plots and Response Correlation Tests --#
-################################################################
+#######################################################
+#-- Correlation Scatter Plots and Correlation Tests --#
+#######################################################
 
-# plot scatter plot
-plot_scatter(data = ESCdata, col_names = all_num)
-# Correlation Test
+# This section generates some scatter plots and performs correlation tests
+# Correlation Test Hypothesis
 # Ho: x is not correlated with y
 # Ha: x is correlated with y
-# execute the correlation tests
-cor_test_df <- corr_tests(data = ESCdata, col_names = all_num)
-# Write the correlation results to  a .csv file
-write.csv(x = cor_test_df, file = "Report/Stats/cor_tests_response.csv", row.names = F)
 
-#############################################
-#-- Predictor Correlation Plots and Tests --#
-#############################################
+# plot scatter plot
+plot_scatter(data = ESCdata, col_names = all_num, output_dir = 'report/plots/scatterplots')
+
+# execute the correlation tests against the response
+resp_cor_tests_df <- corr_tests(data = ESCdata, col_names = all_num)
+# Write the correlation results to  a .csv file
+write.csv(x = resp_cor_tests_df, file = "report/stats/cor_tests_response.csv", row.names = F)
 
 # run correlation tests for all predictors
-c.t. <- pred_corr_tests(ESCdata[all_num])
-
+pred_cor_tests_df <- pred_corr_tests(ESCdata[all_num])
 # output results as a .csv file
-write.csv(x = c.t., file = "Report/Stats/cor_tests_predictors.csv", row.names = F)
+write.csv(x = pred_cor_tests_df, file = "Report/Stats/cor_tests_predictors.csv", row.names = F)
 
 # Determine which variables are highly correlated
-c.t.[c.t.$Correlation > 0.8 & c.t.$Correlation < 1,]
+pred_cor_tests_df[pred_cor_tests_df$Correlation > 0.8,]
 
 #######################
 #-- Social Networks --#
 #######################
+
+# A variety of Social Networks based on different closeness metrics given a specified Voting_Method
+# Here I shall explore the effect of the televote and the Jury Vote
+# For example see below a high diaspora graph
 
 # NOTE: that there is more data missing for METIC_Citizens than METRIC_COB
 sum(!is.na(ESCdata$METRIC_COB))
 sum(!is.na(ESCdata$METRIC_Citizens))
 networkdata <- ESCdata %>% filter(!is.na(METRIC_Citizens))
 
-# A variety of Social Networks based on different closeness metrics given a specified Voting_Method
-# Here I shall explore the effect of the televote and the Jury Vote
-# For example see below a high diaspora graph
 # filter out records for high diaspora in the televote
 high_diaspora_df <- networkdata %>% filter(Voting_Method == "T", METRIC_COB > 0.1)
 # Construct Social Network
@@ -168,7 +149,7 @@ G <- graph_from_data_frame(d = high_diaspora_df[, c('From_country', 'To_country'
 E(G)$weight <- as.numeric(high_diaspora_df[, 'Points'])
 is_weighted(graph = G)
 # plot graph of high diaspora
-plot(G, 
+plot(x = G, 
      main = "Graph of High Diaspora & Points in Televote", 
      layout = layout.fruchterman.reingold, 
      edge.color = "grey", 
