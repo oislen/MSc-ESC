37e500834c4d12a2be5f6685cde7f796c0182003
oislen <oisin.leonard@gmail.com>
2021-06-10 10:16:20 +0100

    Added note about manually downloading data and add code to check duplicated column

--- Proj_2021/scripts/extract_csv_from_xlsx.py
+++ Proj_2021/scripts/extract_csv_from_xlsx.py
@@ -13,11 +13,15 @@ import seaborn as sns
 # set column names
 col_names = ['year', 'round', 'edition', 'voting', 'from_country', 'to_country', 'points', 'duplicated']
 
+# data can be manually downloaded via kaggle or data.world (requires sign-in)
+# https://www.kaggle.com/datagraver/eurovision-song-contest-scores-19752019
+# https://data.world/datagraver/eurovision-song-contest-scores-1975-2019
 # load in excel data
 data = pd.read_excel(cons.raw_xlsx_fpath,
                      sheet_name = 'Data',
                      header = 0,
-                     names = col_names
+                     names = col_names,
+                     engine = 'openpyxl'
                      )
 
 # output data as feather file
@@ -25,6 +29,8 @@ data.to_feather(cons.raw_feather_fpath)
 data.isnull().sum()
 
 # investogate duplicated column
+sub_cols = ['year', 'round', 'voting', 'from_country','to_country', 'points']
+data.loc[:,sub_cols].duplicated().any()
 dup_data = data[data['duplicated'] == 'x']
 dup_data.astype(str).apply(lambda x: '_'.join(x), axis = 1).nunique() / dup_data.shape[0] * 100
 # investigate unique values across all clumns bar duplicate
