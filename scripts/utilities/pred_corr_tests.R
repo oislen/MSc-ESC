c95f8aba2010c9f3d46a83485c83aef5e525ef51
Oisin <oisin.leonard@gmail.com>
2022-09-09 11:25:11 +0100

    Parameterised na.action, digits and sign level. Replaced correlation calculation with corrleation test estimate, and removed row names from output data frame

--- scripts/utilities/pred_corr_tests.R
+++ scripts/utilities/pred_corr_tests.R
@@ -1,33 +1,29 @@
 # IMPORTANT: Correlation does not imply causality
 # Define a function to perform Correlation Tests
-pred_corr_tests <- function (dataset) {
+pred_corr_tests <- function (dataset, na.action = "na.omit", digits = 5, sign_level = 0.05) {
   # extract the unique column names
   n_cols = ncol(dataset)
   dataset_cols = unique(colnames(dataset))
-  cor_test_df <- as.data.frame(matrix(ncol = 4))
+  cor_test_df <- as.data.frame(matrix(ncol = 5))
   # Name the columns of the Correlation Test Data Frame
-  colnames(cor_test_df) <- c("X", "Y", "Correlation", "P-Value")
+  colnames(cor_test_df) <- c("X", "Y", "Correlation", "P-Value", "Significant")
   # Create a row index to populate the data frame with
   r = 1
   for (i in 1:n_cols) {
     j = i + 1
     while (j <= n_cols) {
       # Perform Correlation Test
-      c.t. <- cor.test(x = dataset[,dataset_cols[i]],
-                       y = dataset[,dataset_cols[j]],
-                       na.action = "na.omit")
+      c.t. <- cor.test(x = dataset[,dataset_cols[i]], y = dataset[,dataset_cols[j]], na.action = na.action)
       # Fill in the X Variable Name
-      cor_test_df[r, 1] <- dataset_cols[i]
+      cor_test_df[r, "X"] <- dataset_cols[i]
       # Fill in the Y Variable Name
-      cor_test_df[r, 2] <- dataset_cols[j]
+      cor_test_df[r, "Y"] <- dataset_cols[j]
       # Fill in the correlation
-      #cor_test_df[r, 3] <- round(c.t.$estimate, digits = 5)
-      cor_test_df[r, 3] <- round(cor(x = dataset[,dataset_cols[i]],
-                                     y = dataset[,dataset_cols[j]],
-                                     use = "complete.obs"), 
-                                 digits = 5)
+      cor_test_df[r, "Correlation"] <- round(c.t.$estimate, digits = digits)
       # Fill in the p-value
-      cor_test_df[r, 4] <- round(c.t.$p.value, digits = 5)
+      cor_test_df[r, "P-Value"] <- round(c.t.$p.value, digits = digits)
+      # Fill in significant result
+      cor_test_df[r, "Significant"] <- ifelse(test = round(c.t.$p.value, digits = digits) < sign_level, yes = TRUE, no = FALSE)
       # Update the row index
       r = r + 1
       # Update the j index
@@ -35,5 +31,6 @@ pred_corr_tests <- function (dataset) {
     }
   }
   # return the cor_test_df
+  row.names(cor_test_df) <- NULL
   return(cor_test_df)
 }
