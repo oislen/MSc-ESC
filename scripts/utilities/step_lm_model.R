4efcffad54f4a97adc8d741c5199aac45d44dcd1
Oisin <oisin.leonard@gmail.com>
2022-09-17 08:35:52 +0100

    Added trace = 0 to hide printed messages

--- scripts/utilities/step_lm_model.R
+++ scripts/utilities/step_lm_model.R
@@ -2,7 +2,7 @@
 source('Scripts/utilities/extract_preds_by_cats.R')
 source('Scripts/utilities/extract_sign_preds.R')
 # define a function to fit a step-wise linear model
-step_lm_model <- function(dataset, pred_cols, direction = 'both', steps = 100){
+step_lm_model <- function(dataset, pred_cols, direction = 'both', steps = 100, trace = 0){
   # create an empty list to hold results
   out_list <- list()
   # fit the model
@@ -13,7 +13,7 @@ step_lm_model <- function(dataset, pred_cols, direction = 'both', steps = 100){
   # create the maximum model
   out_list$max_model <- formula(lm(out_list$model_form, data = dataset))
   # my_model_overall <- step(min_model, direction = 'forward', scope = max_model, steps = 100)
-  out_list$my_model_overall <- step(out_list$min_model, direction = direction, scope = out_list$max_model, steps = steps)
+  out_list$my_model_overall <- step(out_list$min_model, direction = direction, scope = out_list$max_model, steps = steps, trace = 0)
   # get model summary
   out_list$summary <- summary(out_list$my_model_overall)
   # get anova results
