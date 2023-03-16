0316e04d703d605b7609e6df0f5e457c4a66c9cb
Oisin <oisin.leonard@gmail.com>
2023-03-16 20:17:55 +0000

    created .bat script to create virtual conda environment with required r packages

---
+++ conda/MSc-ESC.bat
@@ -0,0 +1,13 @@
+:: list all available conda environments
+call conda env list
+
+:: create and activate new R environment
+call conda env remove --name MSc-ESC
+call conda env list
+call conda create -n MSc-ESC r-base
+call conda activate MSc-ESC
+call conda list
+
+:: install all relevant R libraries
+call conda config --add channels r
+call conda install -c r --file ..\requirements.txt --yes
