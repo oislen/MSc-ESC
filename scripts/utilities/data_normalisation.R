b5295a4858877c1f6714dc30713c99282182fe92
Oisin <oisin.leonard@gmail.com>
2025-02-26 07:39:57 +0000

    Added docstrings to data normalisation and reduction functions

--- scripts/utilities/data_normalisation.R
+++ scripts/utilities/data_normalisation.R
@@ -1,4 +1,11 @@
-# (2) Define the data normalization
+#' Data Normalisation
+#' 
+#' @description Normalises all data in a dataframe to have 0 mean and standard deviation 1
+#' 
+#' @param dataset A dataframe with numeric columns to normalise
+#' 
+#' @return Returns the normalised numeric dataframe
+#' 
 data_normalisation <- function(dataset) {
   # range standardization [lower bound, upper_bound], e.g. [0, 1]
   # Convert data to have minimum 0 and maximum 1
--- scripts/utilities/data_reduction_chisq.R
+++ scripts/utilities/data_reduction_chisq.R
@@ -1,15 +1,23 @@
-# load relevant libraries
 library('dplyr')
-# load custom chi-sq test function
-source("../scripts/utilities/chisq_tests.R")
-# define function to apply chi-sq tests to given data and columns
-data_reduction_chisq <- function(dataset, col_names){
+source("utilities/chisq_tests.R")
+
+#' Data Reduction Chi-Square
+#' 
+#' @description Removes columns from a dataframe based on chi-squared tests of associations
+#' 
+#' @param dataset A dataframe with categorical columns
+#' @param col_names The categorical columns to run chi-squared tests of associations
+#' @param p_value The p value significance level to retain categorical columns with
+#' 
+#' @return Returns a dataframe with any columns having non-significant chi-square tests removed
+#' 
+data_reduction_chisq <- function(dataset, col_names, p_value = 0.05){
   # Subset the data to be tested
   sub_df <- dataset %>% subset(select = col_names)
   # Run the Chi-Squared Tests
   chisq_tests <- chisq_tests(dataset = sub_df)
   # Filter out the significant p-values
-  chisq_tests_sign <- chisq_tests[chisq_tests$'P-Value' < 0.05,]
+  chisq_tests_sign <- chisq_tests[chisq_tests$'P-Value' < p_value,]
   # extract out the column names for significant columns
   chisq_test_remove_cols <- unique(chisq_tests_sign[,'Y'])
   # return chi-sq columns to remove
